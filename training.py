ce3bac3322132dcc7cbb222e15a005a3eca8654b
frank731 <bobbyflip332@gmail.com>
2023-07-27 12:47:37 -0600

    Removed unneeded files

--- training.py
+++
@@ -1,8 +0,0 @@
-from blackjack import BlackjackEnv
-from stable_baselines3 import A2C
-from stable_baselines3.common.env_checker import check_env
-
-env = BlackjackEnv("console")
-model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10_000)
-check_env(env)
